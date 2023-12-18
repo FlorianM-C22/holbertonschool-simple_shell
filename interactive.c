388dc8bad940ba5d8b3bd3cf02fdcbe619e67876
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-18 23:51:35 +0100

    Deleted lines in interactive.c to get less than 40

--- interactive.c
+++ interactive.c
@@ -29,17 +29,14 @@ void interactive(void)
 		input_length = strlen(input);
 
 		if (input_length > 0 && input[input_length - 1] == '\n')
-		{
 			input[input_length - 1] = '\0';
-		}
+
 		if (strcmp(input, "exit") == 0)
-		{
 			break;
-		}
+
 		else if (strcmp(input, "env") == 0)
-		{
 			def_env();
-		}
+
 		else
 		{
 			command = strtok(input, " ");
