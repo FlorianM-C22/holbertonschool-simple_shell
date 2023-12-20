d34fd61acc6b6c9b3f914fdfcd3be4e5369a820a
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-20 10:36:00 +0100

    Deleted unnecessary libraries

--- shell.h
+++ shell.h
@@ -4,13 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <errno.h>
 
 extern char **environ;
 
