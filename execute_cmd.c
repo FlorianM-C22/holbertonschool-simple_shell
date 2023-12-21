cd44b4bba3630e1682cd9b2790cb5bfd887e1ccb
FlorianM-C22 <7484@holbertonstudents.com>
2023-12-21 18:31:02 +0100

    Split exec_command.c to have less than 40 lines by function Proper man_1_simple_shell : OK

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,67 +1,98 @@
 #include "shell.h"
 
 /**
+ * file_exists - checks if file exists
+ * @filename: name of the file
+ * Return: 0 SUCCESS
+ */
+int file_exists(char *filename)
+{
+	struct stat filestats;
+
+	if (stat(filename, &filestats) >= 0)
+	{
+		if (filestats.st_mode & (S_IXUSR | S_IXGRP | S_IXOTH))
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * get_command - gets the command
+ * @path: PATH variable
+ * @command: name of the command
+ * Return: Command if SUCCESS
+ */
+char *get_command(char *path, char *command)
+{
+	char *token = NULL;
+	char *full_path = NULL;
+
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (file_exists(command))
+			return (strdup(command));
+		else
+			return (NULL);
+	}
+	token = strtok(path, ":");
+	while (token != NULL)
+	{
+		full_path = malloc(strlen(token) + strlen(command) + 2);
+		if (full_path == NULL)
+		{
+			perror("malloc");
+			return (NULL);
+		}
+		strcpy(full_path, token);
+		strcat(full_path, "/");
+		strcat(full_path, command);
+		if (file_exists(full_path))
+		{
+			return (full_path);
+		}
+		free(full_path);
+		full_path = NULL;
+		token = strtok(NULL, ":");
+	}
+	return (NULL);
+}
+
+/**
  * execute_cmd - command executor
  * @command: command to execute
  * @args: arguments
+ * @data: data struct to store values like exit values
  * Return: void
  */
 int execute_cmd(char *command, char *args[], data_t *data)
 {
-	pid_t pid; /*Declaring a Parent id*/
-	int result, status; /*Index values*/
+	int result; /*Index values*/
 	int i;
+	char *path = NULL; /*Declaring a PATH variable*/
 
-	pid = fork();
 	data->exit_status = 0; /*Setting exit_status to 0*/
-	if (pid == -1)
-	{	/*If forking failed == ERROR*/
-		perror("fork");
-		return (-1);
+
+	path = _getenv("PATH", environ); /*Getting PATH data in a string*/
+
+	if (path == NULL)
+	{	/*If PATH not found = ERROR*/
+		fprintf(stderr, "PATH variable not found\n");
+		exit(EXIT_FAILURE);
 	}
-	else if (pid == 0)
+	/*Search the command and execute it*/
+	result = search_and_exec(command, args, path, data);
+	/*If command not executed = ERROR*/
+	free(path);
+	if (result == -1)
+		exit(EXIT_FAILURE);
+	data->exit_status = result;
+	for (i = 0; args[i]; i++)
 	{
-		char *path = _getenv("PATH", environ); /*Getting PATH data in a string*/
-
-		if (path == NULL)
-		{	/*If PATH not found = ERROR*/
-			fprintf(stderr, "PATH variable not found\n");
-			exit(EXIT_FAILURE);
-		}
-		/*Search the command and execute it*/
-		result = search_and_exec(command, args, path, data);
-		/*If command not executed = ERROR*/
-		free(path);
-		if (result == -1)
-			exit(EXIT_FAILURE);
-		data->exit_status = result;
-for (i = 0; args[i]; i++)
-{
-	free(args[i]);
-	args[i] = NULL;
-}
-
-		_exit(result); /*Exit with the result*/
+		free(args[i]);
+		args[i] = NULL;
 	}
-	else
-	{	/*Parent process*/
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			perror("waitpid");
-			return (-1);
-		}
-		if (WIFEXITED(status))
-		{
-			result = WEXITSTATUS(status);
-			if (data->exit_status == 0)
-				data->exit_status = status / 256;
-		}
-		else
-		{
-			fprintf(stderr, "Child process did not terminate normally\n");
-			result = -1;
-		}
-	}
+
 	return (result);
 }
 
@@ -70,86 +101,35 @@ for (i = 0; args[i]; i++)
  * @command: command to execute
  * @args: arguments
  * @path: PATH variable
+ * @data: data struct to store values like exit values
  * Return: 0 on success, -1 on failure
  */
 int search_and_exec(char *command, char *args[], char *path, data_t *data)
 {
-	char *token, *path_copy, *full_path;
 	int result = -1;
-	struct stat file_stat;
+	int status = 0;
+	char *full_command = NULL;
 
-	if (stat(command, &file_stat) == 0)
+	full_command = get_command(path, command);
+	if (full_command == NULL)
 	{
-		if (!S_ISREG(file_stat.st_mode) || !(file_stat.st_mode & S_IXUSR))
-		{
-			fprintf(stderr, "%s: %d: %s: Permission denied\n", data->argv[0], data->command_count, command);
-			data->exit_status = 126;
-			return (result);
-		}
-		if (execve(command, args, environ) != -1)
-			return (EXIT_SUCCESS);
-		else
-		{
-			perror("execve");
-			return (EXIT_GENERAL_ERROR);
-		}
+		fprintf(stderr, "%s: %d: %s: not found\n", data->argv[0],
+				data->command_count, command);
+		return (127);
 	}
-	path_copy = strdup(path);
-
-	if (path_copy == NULL)
+	else if (!fork() && execve(full_command, args, environ) == -1)
 	{
-		perror("strdup");
-		return EXIT_GENERAL_ERROR;
-	}
-
-	token = strtok(path_copy, ":");
-
-	while (token != NULL)
-	{
-		full_path = malloc(strlen(token) + strlen(command) + 2);
-
-		if (full_path == NULL)
-		{
-			perror("malloc");
-			free(path_copy);
-			return (EXIT_GENERAL_ERROR);
-		}
-		strcpy(full_path, token);
-		strcat(full_path, "/");
-		strcat(full_path, command);
-
-		if (stat(full_path, &file_stat) == 0)
-		{
-			if (!S_ISREG(file_stat.st_mode) || !(file_stat.st_mode & S_IXUSR))
-			{
-				fprintf(stderr, "%s: %d: %s: Permission denied\n", data->argv[0], data->command_count, command);
-				result = 126;
-				break;
-			}
-			if (execve(full_path, args, environ) != -1)
-			{
-				result = EXIT_SUCCESS;
-				break;
-			}
-			else {
-				result = EXIT_BUILTIN_ERROR;
-			}
-		}
-
-		free(full_path);
-		full_path = NULL;
-		token = strtok(NULL, ":");
-	}
-	if (full_path != NULL)
-	{
-
-		free(full_path);
+		fprintf(stderr, "%s: %d: %s: not found\n", data->argv[0],
+				data->command_count, command);
+		data->exit_status = 127;
+		result = 127;
 	}
 	else
 	{
-		fprintf(stderr, "%s: %d: %s: not found\n", data->argv[0], data->command_count, command);
-		result = 127;
+		wait(&status);
+		data->exit_status = status / 256;
+		result = status / 256;
 	}
-	free(path_copy);
+	free(full_command);
 	return (result);
 }
